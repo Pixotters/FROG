f2b19dc0e82966c6f84ff2a73d9d179b4fd80c97
Lasconik <caillouxnicolas@gmail.com>
2014-02-21 14:03:08 +0100

    developed the scene
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- examples/dodger/src/Scene.cpp
+++ examples/dodger/src/Scene.cpp
@@ -1,1 +1,31 @@
 #include "Scene.hpp"
+
+#include "Player.hpp"
+
+Scene::Scene()
+  : State()
+{
+  m_entities.push_back(new Player);
+}
+
+Scene::~Scene()
+{
+
+}
+
+void Scene::draw(sf::RenderTarget& rt, sf::RenderStates rs) const
+{
+  for(auto it = m_entities.begin(); it != m_entities.end(); ++it)
+    {
+      (*it)->draw(rt, rs);
+    }
+}
+
+void Scene::update(const sf::Time& dt)
+{
+  for(auto it = m_entities.begin(); it != m_entities.end(); ++it)
+    {
+      (*it)->update(dt);
+    }
+}
+
