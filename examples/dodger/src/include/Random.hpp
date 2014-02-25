3255ad4ef4c447a87ebed0ce603c58ea94c2cb99
Lasconik <caillouxnicolas@gmail.com>
2014-02-25 01:38:46 +0100

    improved Random to handle negative ints
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

---
+++ examples/dodger/src/include/Random.hpp
@@ -0,0 +1,16 @@
+#ifndef RANDOM_HPP
+#define RANDOM_HPP
+
+
+class Random{
+
+private:
+  static unsigned int sNumbers[100];
+
+public:
+  static void init();
+  static int get(const int& min, const int& max);
+
+};
+
+#endif
