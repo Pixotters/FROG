4d716e16d0d4789ef98695103b0ff44dcb1ee048
Lasconik <caillouxnicolas@gmail.com>
2014-02-26 23:00:05 +0100

    re-added Random that messed up during merge
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

---
+++ examples/dodger/src/Random.cpp
@@ -0,0 +1,27 @@
+#include "Random.hpp"
+
+#include <cstdlib>
+#include <cstdlib>
+#include <ctime>
+
+#include <iostream> // delete
+
+unsigned int Random::sNumbers[100];
+
+void Random::init(){
+  std::srand ( std::time(NULL) );
+  for (unsigned char c =0; c < 100; c++)
+    sNumbers[c] = rand();
+}
+
+ int Random::get(const int& min, const int& max){
+  unsigned int index = (unsigned int)(rand()%100);
+  int res = sNumbers[index];
+  sNumbers[index] = rand();
+  if(min < 0){
+    return (res%(max-min) )+min;
+  }else{
+  std::cout << "random : "<<(res%max)+min<<" in ["<<min<<':'<<max<<"]"<<std::endl;
+  return (res%max)+min;
+  }
+}
