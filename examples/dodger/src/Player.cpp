6f6dc474feb061360c1f6a40cafb32e57e9a69fe
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-03-06 21:40:40 +0100

    added a goal
    Signed-off-by: Nicolas Cailloux <caillouxnicolas@gmail.com>

--- examples/dodger/src/Player.cpp
+++ examples/dodger/src/Player.cpp
@@ -11,7 +11,7 @@ Player::Player(const unsigned short& l)
   m_score = 0;
   static float rad = 24.0f;
   m_transform.setPosition(400, 560 );
-  m_boundingBox = new sf::CircleShape(rad);
+  m_boundingBox = new sf::CircleShape(rad, 6);
   m_boundingBox->setFillColor(sf::Color::Blue);
   m_boundingBox->setOutlineColor(sf::Color::White);
   m_boundingBox->setOutlineThickness(3);
