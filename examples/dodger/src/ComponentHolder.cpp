7215923db1e0b268ca1391f08d562c7de21dc3a5
Lasconik <caillouxnicolas@gmail.com>
2014-03-18 22:27:05 +0100

    Component can be deleted
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- examples/dodger/src/ComponentHolder.cpp
+++ examples/dodger/src/ComponentHolder.cpp
@@ -30,3 +30,17 @@ void ComponentHolder::addComponent(C * c){
   m_components.push_front(c);
 }
 
+
+template <typename C>
+void ComponentHolder::removeComponent(){
+  auto end = m_components.end();
+  for(auto it = m_components.begin(); it != end; it++)
+    {
+      if( dynamic_cast<C *>( *it )  )
+        {
+          m_components.erase(it);
+          break;
+        }
+    }
+}
+
--- examples/dodger/src/include/ComponentHolder.hpp
+++ examples/dodger/src/include/ComponentHolder.hpp
@@ -19,6 +19,8 @@ public:
   C * getComponent() const;
   template <typename C>
   void addComponent(C *);
+  template <typename C>
+  void removeComponent();
 };
 
 #endif
