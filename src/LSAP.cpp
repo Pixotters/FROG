65f94b6111dff3b98e061d634b28917e874a7a89
Julien Sagot <ju.sagot@gmail.com>
2014-03-27 15:57:50 +0100

    Code from hpp to cpp.

--- src/LSAP.cpp
+++ src/LSAP.cpp
@@ -27,13 +27,6 @@ namespace sap {
         delete yAxis;
     }
 
-
-    /**
-     * Create a bounding box attached to a Collisionable and insert its
-     * points in xAxis and yAxis (keep it sorted)
-     * @param c Object (Collisionable) attached to the new bounding box
-     *          to insert in the list
-     */
     void LSAP::addObject(Collisionable * c) {
 
         AABB * aabb = new AABB(c);
@@ -55,13 +48,6 @@ namespace sap {
         updateObject(c);
     }
 
-
-    /**
-     * Update Collisionable's EndPoints position in CollisionManger. Detect 
-     * collisions and separation and act according to actionManager. Should 
-     * be called as soon as an object moves.
-     * @param c Object to update
-     */
     void LSAP::updateObject(Collisionable * c) {
         AABB * aabb = static_cast<AABB *>(c->getBoundingBox());
         aabb->updateEPValues();
@@ -69,14 +55,139 @@ namespace sap {
         updateAxis(aabb->min[1], aabb->max[1]);
     }
   
-  
-    /**
-     * Remove a bounding box attached to a Collisionable 
-     * @param c Object attached to the bounding box to remove
-     */
     void LSAP::removeObject(Collisionable * c) {
         delete static_cast<AABB *>(c->getBoundingBox());
     }
 
+    void LSAP::swap(EndPoint * p1, EndPoint * p2) {
+        if (p2->next != NULL) p2->next->prev = p1;
+        if (p1->prev != NULL) p1->prev->next = p2;
+        p1->next = p2->next;
+        p2->next = p1;  
+        p2->prev = p1->prev;
+        p1->prev = p2;            
+    }
 
+    bool LSAP::partialCollisionCheck(const AABB & a, const AABB & b, int dim) {
+        return !(a.min[dim]->value > b.max[dim]->value
+                 || a.max[dim]->value < b.min[dim]->value);
+    }
+
+    bool LSAP::collisionCheck(const AABB & b1, const AABB & b2) {
+        return partialCollisionCheck (b1, b2, 0)
+            && partialCollisionCheck (b1, b2, 1);
+    }
+
+    void LSAP::updateAxis(EndPoint * min, EndPoint * max) {
+        
+        auto update =
+            [this]
+            (EndPoint * pt,
+             std::function<EndPoint*(EndPoint*)>succ,
+             std::function<bool(EndPoint*pt,EndPoint*succ)>loop_cond,
+             std::function<void(EndPoint*pt,EndPoint*succ)>swap_fun,
+             std::function<bool(EndPoint*pt,EndPoint*succ)>doCollide,
+             std::function<bool(EndPoint*pt,EndPoint*succ)>doSeparate) {
+          
+            EndPoint * tmp = succ(pt);
+            if (!loop_cond(pt, tmp))
+                { return false; }
+
+            do {
+                swap_fun(pt, tmp);
+
+                if (doCollide(pt, tmp)) {
+                    if (this->collisionCheck(*(pt->owner), *(tmp->owner))) {
+                        this->actionManager->onCollision(pt->owner->owner,
+                                                         tmp->owner->owner);
+                    }
+                } else if (doSeparate(pt, tmp)) {
+                    this->actionManager->onSeparation(pt->owner->owner,
+                                                      tmp->owner->owner);
+                }
+
+                tmp = succ(pt);
+            } while (loop_cond(pt, tmp));
+            
+            return true;
+        };
+
+        /* No collision detected on equality (<=/>=). */
+
+        auto prev = [](EndPoint *p)
+            { return p->prev;};
+        auto prev_cond = [](EndPoint * pt, EndPoint * succ)
+            { return pt->value < succ->value; };
+        auto prev_swap = [this](EndPoint * pt, EndPoint * succ)
+            { swap(succ, pt); };
+        auto prev_col = [](EndPoint *pt, EndPoint *succ)
+            { return pt->isMin && !succ->isMin; };
+        auto prev_sep = [](EndPoint *pt, EndPoint *succ)
+            { return !pt->isMin && succ->isMin; };
+                     
+        auto next = [](EndPoint *p)
+            { return p->next; };
+        auto next_cond = [](EndPoint * pt, EndPoint * succ)
+            { return pt->value > succ->value; };
+        auto next_swap = [this](EndPoint * pt, EndPoint * succ)
+            { swap(pt, succ); };
+        auto next_col = [](EndPoint *pt, EndPoint *succ)
+            { return !pt->isMin && succ->isMin; };
+        auto next_sep = [](EndPoint *pt, EndPoint *succ)
+            { return pt->isMin && !succ->isMin; };
+        
+        /* BINOR force update and allow to use a boolean test */
+        if(!(update(max, next, next_cond, next_swap, next_col, next_sep) |
+             update(min, next, next_cond, next_swap, next_col, next_sep))) {
+            update(min, prev, prev_cond, prev_swap, prev_col, prev_sep);
+            update(max, prev, prev_cond, prev_swap, prev_col, prev_sep);
+        }
+
+    }
+
+
+    /* BEGIN LSAP::AABB CLASS */
+
+    LSAP::AABB::AABB(Collisionable * c) : owner(c) {
+        min[0] = new EndPoint(this, c->getXMin(), true);
+        min[1] = new EndPoint(this, c->getYMin(), true);
+        max[0] = new EndPoint(this, c->getXMax(), false);
+        max[1] = new EndPoint(this, c->getYMax(), false);
+        c->setBoundingBox(this);
+    }
+
+    LSAP::AABB::~AABB () { 
+        delete min[0];
+        delete min[1];
+        delete max[0];
+        delete max[1];
+        if (owner) {
+            //   owner->setBoundingBox(NULL); <- segfault in unit tests.
+        }
+    }
+
+    void LSAP::AABB::updateEPValues() {
+        min[0]->value = owner->getXMin();
+        min[1]->value = owner->getYMin();
+        max[0]->value = owner->getXMax();
+        max[1]->value = owner->getYMax();
+    }
+
+    /* END LSAP::AABB CLASS */
+
+    /* BEGIN LSAP::EndPoint CLASS */
+
+    LSAP::EndPoint::EndPoint (AABB* o,int v,bool m, EndPoint* p,EndPoint* n) :
+        owner(o), value(v), isMin(m), prev(p), next(n) {}
+    
+    LSAP::EndPoint::~EndPoint () {
+        if (this->prev != NULL) {
+            this->prev->next = this->next;
+        }
+        if (this->next != NULL) {
+            this->next->prev = this->prev;
+        }
+    }
+
+    /* END LSAP::EndPoint CLASS */
 }
--- src/LSAP.hpp
+++ src/LSAP.hpp
@@ -48,18 +48,10 @@ namespace sap {
             /** Next EndPoint in the list */
             EndPoint * next;
 
-            EndPoint (AABB* o,int v,bool m,EndPoint* p=NULL,EndPoint* n=NULL) : 
-                owner(o), value(v), isMin(m), prev(p), next(n) {}
+            EndPoint (AABB* o,int v,bool m,EndPoint* p=NULL,EndPoint* n=NULL);
     
             /** When and EndPoint is destroyed, it updates prev and next */
-            ~EndPoint () {
-                if (this->prev != NULL) {
-                    this->prev->next = this->next;
-                }
-                if (this->next != NULL) {
-                    this->next->prev = this->prev;
-                }
-            }
+            ~EndPoint ();
         };
     
         class AABB {
@@ -79,38 +71,15 @@ namespace sap {
              * Create the AABB corresponding to a collisionable object
              * @param c the object used to create the corresponding AABB
              */
-            AABB(Collisionable * c) : owner(c) {
-                min[0] = new EndPoint(this, c->getXMin(), true);
-                min[1] = new EndPoint(this, c->getYMin(), true);
-                max[0] = new EndPoint(this, c->getXMax(), false);
-                max[1] = new EndPoint(this, c->getYMax(), false);
-                c->setBoundingBox(this);
-            }
+            AABB(Collisionable * c);
 
-            /**
-             * When a AABB is destroyed, its owner bounding box is set to NULL
-             */
-            ~AABB () { 
-                delete min[0];
-                delete min[1];
-                delete max[0];
-                delete max[1];
-                if (owner) {
-                    //   owner->setBoundingBox(NULL); <- segfault in unit tests.
-                }
-            }
-
+            ~AABB ();
 
             /**
              * Update EndPoints values according to information provided by
              * owner and Collisionable API
              */
-            void updateEPValues() {
-                min[0]->value = owner->getXMin();
-                min[1]->value = owner->getYMin();
-                max[0]->value = owner->getXMax();
-                max[1]->value = owner->getYMax();
-            }
+            void updateEPValues();
 
         };
 
@@ -132,107 +101,26 @@ namespace sap {
          * @param p1 first EndPoint
          * @param p2 second EndPoint
          */
-        void swap(EndPoint * p1, EndPoint * p2) {
-            if (p2->next != NULL) p2->next->prev = p1;
-            if (p1->prev != NULL) p1->prev->next = p2;
-            p1->next = p2->next;
-            p2->next = p1;  
-            p2->prev = p1->prev;
-            p1->prev = p2;            
-        }
+        void swap(EndPoint * p1, EndPoint * p2);
 
         /**
          * A collision between two AABB on one axis
          * no collision on equality (>/<)
          */
-        inline bool partialCollisionCheck(const AABB & a,
-                                          const AABB & b,
-                                          int dim) {
-            return !(a.min[dim]->value > b.max[dim]->value
-                     || a.max[dim]->value < b.min[dim]->value);
-        }
+        bool partialCollisionCheck(const AABB & a, const AABB & b, int dim);
 
         /**
          * Check if two AABB collide using 2 axises
          * @see partialCollisionCheck
          */
-        bool collisionCheck(const AABB & b1, const AABB & b2) {
-            return partialCollisionCheck (b1, b2, 0)
-                && partialCollisionCheck (b1, b2, 1);
-        }
-
+        bool collisionCheck(const AABB & b1, const AABB & b2);
 
         /** Update EndPoint place and call the appropriate function in case 
          * of collision/separation. Does not affect the EndPoint.value, which 
          * must be updated before calling updateEndPoint.
          * @param pt the EndPoint to update
          */
-        void updateAxis(EndPoint * min, EndPoint * max) {
-        
-            auto update =
-                [this]
-                (EndPoint * pt,
-                 std::function<EndPoint*(EndPoint*)>succ,
-                 std::function<bool(EndPoint*pt,EndPoint*succ)>loop_cond,
-                 std::function<void(EndPoint*pt,EndPoint*succ)>swap_fun,
-                 std::function<bool(EndPoint*pt,EndPoint*succ)>doCollide,
-                 std::function<bool(EndPoint*pt,EndPoint*succ)>doSeparate) {
-          
-                EndPoint * tmp = succ(pt);
-                if (!loop_cond(pt, tmp))
-                    { return false; }
-
-                do {
-                    swap_fun(pt, tmp);
-
-                    if (doCollide(pt, tmp)) {
-                        if (this->collisionCheck(*(pt->owner), *(tmp->owner))) {
-                            this->actionManager->onCollision(pt->owner->owner,
-                                                             tmp->owner->owner);
-                        }
-                    } else if (doSeparate(pt, tmp)) {
-                        this->actionManager->onSeparation(pt->owner->owner,
-                                                          tmp->owner->owner);
-                    }
-
-                    tmp = succ(pt);
-                } while (loop_cond(pt, tmp));
-            
-                return true;
-            };
-
-            /* No collision detected on equality (<=/>=). */
-
-            auto prev = [](EndPoint *p)
-                { return p->prev;};
-            auto prev_cond = [](EndPoint * pt, EndPoint * succ)
-                { return pt->value < succ->value; };
-            auto prev_swap = [this](EndPoint * pt, EndPoint * succ)
-                { swap(succ, pt); };
-            auto prev_col = [](EndPoint *pt, EndPoint *succ)
-                { return pt->isMin && !succ->isMin; };
-            auto prev_sep = [](EndPoint *pt, EndPoint *succ)
-                { return !pt->isMin && succ->isMin; };
-                     
-            auto next = [](EndPoint *p)
-                { return p->next; };
-            auto next_cond = [](EndPoint * pt, EndPoint * succ)
-                { return pt->value > succ->value; };
-            auto next_swap = [this](EndPoint * pt, EndPoint * succ)
-                { swap(pt, succ); };
-            auto next_col = [](EndPoint *pt, EndPoint *succ)
-                { return !pt->isMin && succ->isMin; };
-            auto next_sep = [](EndPoint *pt, EndPoint *succ)
-                { return pt->isMin && !succ->isMin; };
-        
-            /* BINOR force update and allow to use a boolean test */
-            if(!(update(max, next, next_cond, next_swap, next_col, next_sep) |
-                 update(min, next, next_cond, next_swap, next_col, next_sep))) {
-                update(min, prev, prev_cond, prev_swap, prev_col, prev_sep);
-                update(max, prev, prev_cond, prev_swap, prev_col, prev_sep);
-            }
-
-        }
+        void updateAxis(EndPoint * min, EndPoint * max);
     
     public:
 
