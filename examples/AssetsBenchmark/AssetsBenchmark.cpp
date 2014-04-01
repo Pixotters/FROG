b6959987c92888bbc691b6f8cd6f0c0e1bca8b0a
Lasconik <caillouxnicolas@gmail.com>
2014-04-01 12:28:56 +0200

    added an example for benchmarking assets loading : see details
    to launch the benchmark, use 'make ARGS="<on/off> <nb>" start' where :
    - <on/off> is 1 if you want to use the AssetManager, 0 if you don't want to
    - <nb> is the number of times the file should be loaded. Please do not enter
    an excessive value in "off" mode (500 takes 12s)
    
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

---
+++ examples/AssetsBenchmark/AssetsBenchmark.cpp
@@ -0,0 +1,56 @@
+#include "AssetManager.hpp"
+#include "GameObject.hpp"
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
+static int LOAD_COUNT = 0;
+
+typedef sf::Texture Tex;
+
+void loadFromFile(const std::string& path, const std::string& id) 
+throw (std::runtime_error)
+{
+  std::unique_ptr<Tex> pr(new Tex() );  
+  LOAD_COUNT++;
+  if( not pr->loadFromFile(path) )
+    {
+      std::string err = "Impossible to create asset because a problem ";
+      err += "problem occured while loading " + path;
+      throw std::runtime_error(err);
+          
+    }
+}
+
+
+
+int main(int charc, char ** argv)
+{  
+  int ceiling = 1000;
+  if(charc >= 3)
+    {
+      ceiling = atoi(argv[2]);
+    }
+  if(charc >= 2)
+    {
+      std::cout << "Performing "<<ceiling<<" loadings "<<std::endl;
+
+      if( * argv[1] == '0')
+        {
+          // no asset manager
+          LOAD_COUNT = 0;
+          for(int i = 0; i < ceiling; i++)
+            loadFromFile("test.png", "a");
+          std::cout<< "Without AssetManager -> "<<LOAD_COUNT<<"/"<<ceiling<<std::endl;
+        }else{
+        // using asset manager
+        frog::AssetManager<std::string, Tex> am;
+        for(int i = 0; i < ceiling; i++)
+          am.loadFromFile("test.png", "a");
+        std::cout<< "With AssetManager -> "<<am.getLoadCount()<<"/"<<ceiling<<std::endl;
+
+      }
+    }
+
+}
