15bc6a484dbbfd84de547c3c123404dda0825865
Julien Sagot <ju.sagot@gmail.com>
2014-04-10 14:19:00 +0200

    Dummy implementation of ActionManager in order to test Physics.

--- demo/demo_BasicPhysics.cpp
+++ demo/demo_BasicPhysics.cpp
@@ -1,61 +1,70 @@
-#include "BasicPhysics.hpp"
 #include <SFML/System/Vector2.hpp>
-#include <cmath.h>
+#include <cmath>
+#include "FROG/Physics/BasicPhysics.hpp"
+#include "FROG/Scene.hpp"
+#include "FROG/GameObject.hpp"
+#include "FROG/Collision/ActionManager.hpp"
 
-class Walls : virtual public PhysicBody {
+using namespace frog;
 
-  int xMin, xMax, yMin, yMax;
+class TestBody : virtual public PhysicBody, virtual public sap::Collisionable {};
 
-  /* dummy class to make circles stay in the sreen */
+class Walls : virtual public TestBody {
+
+    /* dummy class to make circles stay in the sreen */
 public:
 
-  Walls (int xi, int xa, int yi, int ya) :
-    xMin(xi), xMax(xa), yMin(yi), yMax(ya) {}
-  ~Walls () {}
+    int xMin, xMax, yMin, yMax;
 
-  virtual sf::Vector2f getNormal(int x, int y) {
+    Walls (int xi, int xa, int yi, int ya) :
+        xMin(xi), xMax(xa), yMin(yi), yMax(ya) {}
+    ~Walls () {}
 
-    sf::Vector2f v(0,0);
+    virtual sf::Vector2f getNormal(int x, int y) {
 
-    if (x <= xMin) {
-      v += sf::Vector2f(0, 1);
-    } else if (x >= xMax) {
-      v += sf::Vector2f(0, -1);
+        sf::Vector2f v(0,0);
+
+        if (x <= xMin) {
+            v += sf::Vector2f(0, 1);
+        } else if (x >= xMax) {
+            v += sf::Vector2f(0, -1);
+        }
+
+        if (y <= yMin) {
+            v += sf::Vector2f(1, 0);
+        } else if (y >= yMax) {
+            v += sf::Vector2f(-1, 0);
+        }
+
+        return v;
+
     }
-
-    if (y <= yMin) {
-      v += sf::Vector2f(1, 0);
-    } else if (y >= Ymax) {
-      v += sf::Vector2f(-1, 0);
-    }
-  }
 
 protected:
-  /* do not react when force applied */
-  virtual void applyForce(const sf::Vector2f & f) {}
+    /* do not react when force applied */
+    virtual void applyForce(const sf::Vector2f & f) {}
 
 };
 
-class Circle : virtual public PhysicBody {
-
-private:
-  int x, y;
-  int r;
+class Circle : virtual public TestBody {
 
 public:
 
-  Circle (int _x, int _y, int _r) : x(_x), y(_y), r(_r) {}
-  ~Circle () {}
+    int x, y;
+    int r;
 
-  virtual sf::Vector2f getNormal(int x, int y) {
+    Circle (int _x, int _y, int _r) : x(_x), y(_y), r(_r) {}
+    ~Circle () {}
 
-    float _x = x - this->x;
-    float _y = y - this->y;
-    float len = sqrt(pow(_x, 2) + pow(_y, 2));
+    virtual sf::Vector2f getNormal(int x, int y) {
 
-    return sf::Vector2f ( _x / len, _y / len);
+        float _x = x - this->x;
+        float _y = y - this->y;
+        float len = sqrt(pow(_x, 2) + pow(_y, 2));
+
+        return sf::Vector2f ( _x / len, _y / len);
         
-  }
+    }
 };
 
 
@@ -70,51 +79,51 @@ class Demo : virtual public Scene
 {
 
 public:
-  Demo(const unsigned int& n) : Scene(){
-    for(unsigned int it = 0; it < n; it++)
-      {
-        unsigned int x = Random::get(0, 800);
-        unsigned int y = Random::get(0, 600);
-        addCircle(x, y, new GCircle() );
-      }
-    m_collider = new sap::LSAP(am);  
-  }
+    Demo(const unsigned int& n) : Scene(){
+        for(unsigned int it = 0; it < n; it++)
+            {
+                unsigned int x = Random::get(0, 800);
+                unsigned int y = Random::get(0, 600);
+                addCircle(x, y, new GCircle() );
+            }
+        m_collider = new sap::LSAP(am);  
+    }
 
-  virtual ~Demo(){}
-  void addCircle(const unsigned int& x, const unsigned int& y, GCircle * c){
-    Transform * t = c->getTransform();
-    t->setPosition(x, y);
-    addObject(c);
-  };
+    virtual ~Demo(){}
+    void addCircle(const unsigned int& x, const unsigned int& y, GCircle * c){
+        Transform * t = c->getTransform();
+        t->setPosition(x, y);
+        addObject(c);
+    };
 };
 
 class GCircle : virtual public GameObject
 {
 
 private:
-  sf::CircleShape m_sprite;
+    sf::CircleShape m_sprite;
 
 public:
-  GCircle(const float& rad = 32,
-          const sf::Color& c = sf::Color::White){
-    m_sprite.setRadius(rad);
-    m_sprite.setColor(c);
-    addComponent<RenderingComponent>(m_sprite);
-  };
-  virtual ~GCircle();
+    GCircle(const float& rad = 32,
+            const sf::Color& c = sf::Color::White){
+        m_sprite.setRadius(rad);
+        m_sprite.setColor(c);
+        addComponent<RenderingComponent>(m_sprite);
+    };
+    virtual ~GCircle();
 };
 
 /////////////////////////:
 
 int main(int argc, char ** argv)
 {
-  int n = 7;
-  if(argc > 1)
-    n = atoi(argv[1]);
-  App::instance()->init(new Demo(n) );
-  Random::init();
-  App::instance()->run();
-  App::instance()->exit();
+    int n = 7;
+    if(argc > 1)
+        n = atoi(argv[1]);
+    App::instance()->init(new Demo(n) );
+    Random::init();
+    App::instance()->run();
+    App::instance()->exit();
 }
 
 /* TODO:
@@ -124,3 +133,36 @@ int main(int argc, char ** argv)
  * - Render scene and relax
  */
 
+class BaPhy : virtual public BasicPhysics {};
+
+class AcMan : virtual public sap::ActionManager {
+
+    /* Yep, this is ugly, but ActionManager needs to be improved */
+
+public:
+
+    BaPhy buck;
+
+    /* FIXME: Dummy collision position is center of first cicle */
+
+    virtual void onCollision(TestBody * b1, TestBody * b2) {
+        /* Collision between circle + something else */
+        Circle * c1 = dynamic_cast<Circle*>(b1);
+
+        if (Circle * c2 = dynamic_cast<Circle*>(b2)) {
+            buck.reaction(c1, c2, c1->x, c1->y);
+        } else if (Walls * w = dynamic_cast<Walls*>(b2)) {
+            buck.reaction(c1, w, c1->x, c1->y);
+        }
+    }
+
+    virtual void onSeparation(TestBody * b1, TestBody * b2) {
+        /* Separation: do nothing except if a Circle try to run out Walls */
+        Circle * c = dynamic_cast<Circle*>(b1);
+        Walls * w = dynamic_cast<Walls*>(b2);
+        if (c != NULL && w != NULL) {
+            buck.reaction(c, w, c->x, c->y);
+        }
+    }
+
+};
