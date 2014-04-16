5e2348d039d7600a7ed438735c0ed2d3f29a9bfa
Julien Sagot <ju.sagot@gmail.com>
2014-04-16 16:10:08 +0200

    WTF was that?!...

--- src/Physics/BasicPhysics.cpp
+++ src/Physics/BasicPhysics.cpp
@@ -1,6 +1,1 @@
 #include "FROG/Physics/BasicPhysics.hpp"
-
-PhysicBody::PhysicBody(const float& x, const float& y) 
-  : force(x, y) {
-  force += sf::Vector2f(x, y);
-}
--- src/include/FROG/Physics/BasicPhysics.hpp
+++ src/include/FROG/Physics/BasicPhysics.hpp
@@ -38,7 +38,7 @@ protected:
 
 public:
 
-    PhysicBody(const float& x = 0, const float& y = 0);
+    PhysicBody(float x = 0, float y = 0) : force(x, y) {}
 
     virtual ~PhysicBody() {}
 
