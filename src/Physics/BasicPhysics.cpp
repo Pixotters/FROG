13396eeb09466858ec00eead8a6c55565de5328f
Julien Sagot <ju.sagot@gmail.com>
2014-04-16 15:59:39 +0200

    Removed "cout <<"...

--- src/Physics/BasicPhysics.cpp
+++ src/Physics/BasicPhysics.cpp
@@ -1,9 +1,6 @@
 #include "FROG/Physics/BasicPhysics.hpp"
 
-#include <iostream> // TODO remove
-
 PhysicBody::PhysicBody(const float& x, const float& y) 
   : force(x, y) {
   force += sf::Vector2f(x, y);
-  std::cout << "force : x,y "<< x << ","<< y<< std::endl;
 }
