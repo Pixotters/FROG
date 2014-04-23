94f927ff8b6d56e35686bccc34ee44443ebe5ec1
Lasconik <caillouxnicolas@gmail.com>
2014-04-23 11:53:45 +0200

    Config now load from a file
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,4 +1,7 @@
 #include "FROG/Config.hpp"
+#include "FROG/XML/tinyxml2.hpp"
+
+#include "FROG/Debug.hpp"
 
 #include <map>
 
@@ -17,9 +20,32 @@ namespace frog{
 
   void Config::loadFromFile(const std::string& file)
   {
-    m_windowWidth = 800;
-    m_windowHeight = 600;
-    m_title = "Dodger";
+    std::cerr << "Config file : "<< file << std::endl;
+    m_windowWidth = 320;
+    m_windowHeight = 240;
+    m_title = "FROG Application";
+    tinyxml2::XMLDocument doc;
+    if( doc.LoadFile( file.c_str() ) == tinyxml2::XML_NO_ERROR ) {
+      std::cerr << "loaded file : "<< file << std::endl;    
+      tinyxml2::XMLElement * basenode = doc.FirstChildElement("configuration");
+      tinyxml2::XMLElement * titlenode = basenode->FirstChildElement("app")->FirstChildElement("title");
+      if ( titlenode ) {    
+        m_title = titlenode->GetText();
+      }
+      tinyxml2::XMLElement * windownode = basenode->FirstChildElement("window");
+      if ( windownode ){
+        tinyxml2::XMLElement * widthnode = windownode->FirstChildElement("width");
+        if ( widthnode) {
+          std::cerr << "width node" << std::endl;
+          widthnode->QueryUnsignedText(&m_windowWidth);
+        }
+        tinyxml2::XMLElement * heightnode = windownode->FirstChildElement("height");
+        if ( heightnode ) {
+          heightnode->QueryUnsignedText(&m_windowHeight);
+        }
+      }      
+        
+    }
   }
 
   unsigned int Config::getWindowWidth() const
