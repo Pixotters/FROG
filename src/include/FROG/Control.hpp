c23a4f115ad19b01bffdd92e49e9349af5b8aaa4
Lasconik <caillouxnicolas@gmail.com>
2014-05-06 12:45:02 +0200

    include wrappers
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- src/include/FROG/Control.hpp
+++ src/include/FROG/Control.hpp
@@ -1,15 +1,13 @@
 #ifndef FROG_CONTROL_HPP
 #define FROG_CONTROL_HPP
 
-#include "FROG/Control/ControlHandler.hpp"
+#include "FROG/Control/ButtonTrigger.hpp"
+#include "FROG/Control/ControlComponent.hpp"
 #include "FROG/Control/Input.hpp"
-#include "FROG/Control/Button.hpp"
+#include "FROG/Control/InputComponent.hpp"
+#include "FROG/Control/JoystickButton.hpp"
 #include "FROG/Control/KeyboardButton.hpp"
 #include "FROG/Control/MouseButton.hpp"
-#include "FROG/Control/JoystickButton.hpp"
-#include "FROG/Control/KeyboardSimpleButton.hpp"
-#include "FROG/Control/MouseSimpleButton.hpp"
-#include "FROG/Control/JoystickSimpleButton.hpp"
 #include "FROG/Control/Xbox.hpp"
 
 #endif
