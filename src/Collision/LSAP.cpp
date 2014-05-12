c08c86764eebc1a24e904d8f791000f4f5630957
Julien Sagot <ju.sagot@gmail.com>
2014-05-12 16:58:05 +0200

    Style + comments.

--- src/Collision/LSAP.cpp
+++ src/Collision/LSAP.cpp
@@ -5,10 +5,11 @@
 
 #include "FROG/Collision/LSAP.hpp"
 
-namespace frog {
+namespace frog
+{
 
   LSAP::LSAP () 
-    : CollisionManager() 
+  : CollisionManager() 
   {
     /* not sure about the true/false values */
     xAxis = new EndPoint(NULL, INT_MIN, true, NULL, NULL);
@@ -17,18 +18,20 @@ namespace frog {
     yAxis->next = new EndPoint(NULL, INT_MAX, false, yAxis, NULL);
   }
 
-  LSAP::~LSAP () {
+  LSAP::~LSAP ()
+  {
     
     /* delete all AABB will delete EndPoints */
-    /*while (xAxis->next != NULL && xAxis->next->owner != NULL) {
-      delete xAxis->next->owner;
-      }*/
     for (auto& couple : m_objects)
       {
         if (couple.second != nullptr)
+        {
           delete couple.second;
+        }
       }
+
     m_objects.clear();
+
     /* delete all sentinels */
     if (xAxis->next != nullptr)
       delete xAxis->next;
@@ -40,9 +43,11 @@ namespace frog {
       delete yAxis;
   }
 
-  void LSAP::addObject(std::shared_ptr<GameObject> go) {
+  void LSAP::addObject(std::shared_ptr<GameObject> go)
+  {
     auto collider = go->getComponent<Collider>("COLLIDER");
     AABB * aabb = new AABB(collider);
+
     m_objects.emplace(go, aabb);
     aabb->min[0]->next = aabb->max[0];
     aabb->max[0]->prev = aabb->min[0];
@@ -61,7 +66,8 @@ namespace frog {
     updateObject(go);
   }
 
-  void LSAP::updateObject(std::shared_ptr<GameObject> go) {
+  void LSAP::updateObject(std::shared_ptr<GameObject> go)
+  {
     auto f = m_objects.find(go);
     if (f != m_objects.end() )
       {
@@ -71,19 +77,19 @@ namespace frog {
       }
   }
   
-  void LSAP::removeObject(std::shared_ptr<GameObject> go) {
+  void LSAP::removeObject(std::shared_ptr<GameObject> go)
+  {
     auto find = m_objects.find(go);
     if (find != m_objects.end() )
       {
-        std::cerr << "removing object "<<go<<"("<<go.get()<<")" \
-                  << "associated with "<< find->second<<std::endl;
         delete find->second;
         m_objects.erase(find);        
       }
     
   }
 
-  void LSAP::swap(EndPoint * p1, EndPoint * p2) {
+  void LSAP::swap(EndPoint * p1, EndPoint * p2)
+  {
     if (p2->next != nullptr) p2->next->prev = p1;
     if (p1->prev != nullptr) p1->prev->next = p2;
     p1->next = p2->next;
@@ -92,97 +98,146 @@ namespace frog {
     p1->prev = p2;            
   }
 
-  bool LSAP::partialCollisionCheck(const AABB & a, const AABB & b, int dim) {
-    std::cerr << "trying with "<< &a <<" & "<<&b<<std::endl;
-    std::cerr << "EPs - a  :"<< a.min[dim] <<" - "<< a.max[dim] <<std::endl;    
-    std::cerr << "b :  "<< b.min[dim] <<" - "<< b.max[dim] <<std::endl;    
-    return !(a.min[dim]->value > b.max[dim]->value
-             || a.max[dim]->value < b.min[dim]->value);
+  bool LSAP::partialCollisionCheck(const AABB & a, const AABB & b, int dim)
+  {
+    return not(a.min[dim]->value > b.max[dim]->value
+               or a.max[dim]->value < b.min[dim]->value);
   }
 
-  bool LSAP::collisionCheck(const AABB & b1, const AABB & b2) {
+  bool LSAP::collisionCheck(const AABB & b1, const AABB & b2)
+  {
     return partialCollisionCheck (b1, b2, 0)
-      && partialCollisionCheck (b1, b2, 1);
+            and partialCollisionCheck (b1, b2, 1);
   }
 
-  void LSAP::updateAxis(EndPoint * min, EndPoint * max) {
+  void LSAP::updateAxis(EndPoint * min, EndPoint * max)
+  {
+
+  /*
+   * succ: function for getting the next EndPoint form another one
+   * loop_cond: condition for loop break testing
+   * swap_fun: function to swap two EndPoints with necessary operations
+   * doCollide: function to tell if two objects which were not colliding
+   *            are colliding now.
+   * doSeparate: function to tell if two objects which were colliding
+   *             are not colliding anymore.
+   */
+
     auto update =
       [this]
       (EndPoint * pt,
-       std::function<EndPoint*(EndPoint*)>succ,
-       std::function<bool(EndPoint*pt,EndPoint*succ)>loop_cond,
-       std::function<void(EndPoint*pt,EndPoint*succ)>swap_fun,
-       std::function<bool(EndPoint*pt,EndPoint*succ)>doCollide,
-       std::function<bool(EndPoint*pt,EndPoint*succ)>doSeparate) {
+       std::function<EndPoint*(EndPoint*)> succ,
+       std::function<bool(EndPoint*pt,EndPoint*succ)> loop_cond,
+       std::function<void(EndPoint*pt,EndPoint*succ)> swap_fun,
+       std::function<bool(EndPoint*pt,EndPoint*succ)> doCollide,
+       std::function<bool(EndPoint*pt,EndPoint*succ)> doSeparate)
+      {
           
-      EndPoint * tmp = succ(pt);
-      if (!loop_cond(pt, tmp))
-        { return false; }
-      do {
-        swap_fun(pt, tmp);
-        if (doCollide(pt, tmp)) {
-          std::cerr << "COLLISION OCCURED ?? " << std::endl;
-          if (this->collisionCheck(*(pt->owner), *(tmp->owner))) {
-            std::cerr << "COLLISION OCCURED " << std::endl;
-            sendCollision(pt->owner, tmp->owner, Collision::COLLISION);
-          }
-        } else if (doSeparate(pt, tmp)) {
-          std::cerr << "SEPARATION OCCURED " << std::endl;
-          sendCollision(pt->owner, tmp->owner, Collision::SEPARATION);
+        EndPoint * tmp = succ(pt);
+
+        if (!loop_cond(pt, tmp))
+        {
+          return false;
         }
-        tmp = succ(pt);
-      } while (loop_cond(pt, tmp));
+
+        do
+        {
+          swap_fun(pt, tmp);
+          if (doCollide(pt, tmp))
+          {
+            if (this->collisionCheck(*(pt->owner), *(tmp->owner)))
+            {
+              sendCollision(pt->owner, tmp->owner, Collision::COLLISION);
+            }
+          }
+          else if (doSeparate(pt, tmp))
+          {
+            sendCollision(pt->owner, tmp->owner, Collision::SEPARATION);
+          }
+          tmp = succ(pt);
+        }
+        while (loop_cond(pt, tmp));
+
       return true;
     };
 
     /* No collision detected on equality (<=/>=). */
 
+    /* BEGIN: "from left to right"  */
+
     auto prev = [](EndPoint *p)
-      { return p->prev;};
-    auto prev_cond = [](EndPoint * pt, EndPoint * succ)
-      {  if (pt != nullptr and succ != nullptr)
-          {
-            return pt->value < succ->value;
-          }else
-          {
-            return false;
-          } 
+      {
+        return p->prev;
       };
+
+    auto prev_cond = [](EndPoint * pt, EndPoint * succ)
+      { 
+        if (pt != nullptr and succ != nullptr)
+        {
+          return pt->value < succ->value;
+        }
+        else
+        {
+          return false;
+        } 
+      };
+
     auto prev_swap = [this](EndPoint * pt, EndPoint * succ)
-      { swap(succ, pt); };
+      {
+        swap(succ, pt);
+      };
+
     auto prev_col = [](EndPoint *pt, EndPoint *succ)
       { 
-        std::cerr << "testing prev collision " << pt->isMin <<"-"<< !succ->isMin<< std::endl;
-        return pt->isMin && !succ->isMin; };
+        return pt->isMin && !succ->isMin;
+      };
+
     auto prev_sep = [](EndPoint *pt, EndPoint *succ)
       { 
-        std::cerr << "testing prev separation " << !pt->isMin <<"-"<< succ->isMin<< std::endl;
-        return !pt->isMin && succ->isMin; };
-                     
+        return !pt->isMin and succ->isMin;
+      };
+    /* END: "from left to right"  */
+
+    /* BEGIN: "from right to left"  */
     auto next = [](EndPoint *p)
-      { return p->next; };
+      {
+        return p->next;
+      };
+
     auto next_cond = [](EndPoint * pt, EndPoint * succ)
-      {  if (pt != nullptr and succ != nullptr)
-          {
-            return pt->value > succ->value;
-          }else
-          {
-            return false;
-          }  };
+      {
+        if (pt != nullptr and succ != nullptr)
+        {
+          return pt->value > succ->value;
+        }
+        else
+        {
+          return false;
+        }
+      };
+
     auto next_swap = [this](EndPoint * pt, EndPoint * succ)
-      { swap(pt, succ); };
+      {
+        swap(pt, succ);
+      };
+
     auto next_col = [](EndPoint *pt, EndPoint *succ)
       { 
-        std::cerr << "testing next collision " << !pt->isMin <<"-"<< succ->isMin<< std::endl;
-        return !pt->isMin && succ->isMin; };
+        return !pt->isMin && succ->isMin;
+      };
+
     auto next_sep = [](EndPoint *pt, EndPoint *succ)
       { 
-        std::cerr << "testing next separation " << pt->isMin <<"-"<< !succ->isMin<< std::endl;
-        return pt->isMin && !succ->isMin; };
-        
-    /* BINOR force update and allow to use a boolean test */
-    if(!(update(max, next, next_cond, next_swap, next_col, next_sep) |
-         update(min, next, next_cond, next_swap, next_col, next_sep))) {
+        return pt->isMin && !succ->isMin;
+      };
+    /* END: "from right to left" */
+
+    /* Test if when need to update the position of the points in the list.
+     * First look at order regarding to successors. If the place still the
+     * same, look for an update according to predecessors
+     * BINOR force update and allow to use a boolean test */
+    if(not(update(max, next, next_cond, next_swap, next_col, next_sep) |
+           update(min, next, next_cond, next_swap, next_col, next_sep))) {
       update(min, prev, prev_cond, prev_swap, prev_col, prev_sep);
       update(max, prev, prev_cond, prev_swap, prev_col, prev_sep);
     }
@@ -191,7 +246,7 @@ namespace frog {
 
   void LSAP::update()
   {
-    for (auto& c : m_objects)
+    for (auto & c : m_objects)
       {
         updateObject(c.first);
       }
@@ -201,16 +256,16 @@ namespace frog {
   {
     std::shared_ptr<GameObject> g1, g2;
     for (auto it: m_objects)
+    {
+      if (it.second == a1)
       {
-        if ( it.second == a1)
-          {
-            g1 = it.first;
-          }        
-        if ( it.second == a2)
-          {
-            g2 = it.first;
-          }
+        g1 = it.first;
+      }        
+      if (it.second == a2)
+      {
+        g2 = it.first;
       }
+    }
     if (g1.get() != nullptr and g2.get() != nullptr
         and a1 != nullptr)
       {
